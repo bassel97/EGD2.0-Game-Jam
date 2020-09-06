4ae5b1672bc0abb1f671e56c6293abc000eff00e
bassel97 <bassel.ahmed97@eng-st.cu.edu.eg>
2020-09-06 17:50:28 +0200

    Added Jump Mechanic

---
+++ BoB the blop/Assets/_Scripts/Mechanics/JumpMechanic.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMechanic : MonoBehaviour
+{
+
+    //**************************** Private Vars *******************************//
+    Rigidbody rb;
+    ConstantForce cf;
+
+    float jumpVelocity;
+    bool isGrounded = false;
+
+    //**************************** Serialized Vars *******************************//
+    [SerializeField] float jumpHeight = 1;
+    [SerializeField] float characterHeight = 1;
+
+    //**************************** Public Vars *******************************//
+
+
+    float CalculateJumpForce(float acc, float disp)
+    {
+        float eqRes1, eqRes2;
+
+        eqRes1 = (1 + Mathf.Sqrt(1 - 4 * 2 * acc * disp)) / 2;
+        eqRes2 = (1 - Mathf.Sqrt(1 - 4 * 2 * acc * disp)) / 2;
+
+        if (eqRes1 > 0)
+            return eqRes1;
+        return eqRes2;
+    }
+
+    void Start()
+    {
+        //Setup
+        rb = GetComponent<Rigidbody>();
+        cf = GetComponent<ConstantForce>();
+
+
+        jumpVelocity = CalculateJumpForce(cf.force.y, jumpHeight);
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, characterHeight / 2 + 0.15f);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity += Vector3.up * jumpVelocity;
+        }
+    }
+}
